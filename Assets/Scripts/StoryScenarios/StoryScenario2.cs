397127bcbfa18da7f2b0bcb90adaf93bfe212f7b
Ong Heng Le <initials.hl@gmail.com>
2022-07-16 00:24:55 -0700

    Add sample StoryScenario

---
+++ Assets/Scripts/StoryScenarios/StoryScenario2.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryScenario2 : MonoBehaviour
+{
+    public override string GetTitle()
+    {
+        return "Infiltrate the dice mines";
+    }
+
+    public override List<string> GetPreRollTextList()
+    {
+        return new List<string> {
+            "On a planet's Dice mines",
+            "In this world, dice provide power and luck to the user, enhancing their abilities.",
+            "Without any dice, no scenario will play out in your favour.",
+            "Sneak into the dice mines and steal the loot"
+        };
+    }
+
+    public override void PerformRollResult(int rolledNumber)
+    {
+        if (rolledNumber <= 3)
+        {
+            //gain X-1 dice
+        }
+        else if (rolledNumber <= 8)
+        {
+            //gain X dice
+        }
+        else
+        {
+            //gain X/2 dice
+        }
+    }
+
+    public override List<string> GetPostRollTextList(int rolledNumber)
+    {
+        List<string> postRollTextList = new List<string>();
+        if (rolledNumber <= 3)
+        {
+            postRollTextList.Add("Successfully stole as many dice as possible before escaping quietly");
+        }
+        else if (rolledNumber <= 8)
+        {
+            postRollTextList.Add("Guards are onto you. Escape despite only stealing a few dice");
+        }
+        else
+        {
+            postRollTextList.Add("Took too much dice and alerted the guards. Dropped half of the stolen dice while escaping");
+        }
+        postRollTextList.Add("That was a good haul of dice. Could have been better, but still good. I should get out of here before the guards find me.");
+    }
+}
