7be42409682d76510e464350d0263c085536b05c
Ong Heng Le <initials.hl@gmail.com>
2022-07-16 15:33:51 -0700

    Show endgame reason. Reset player resources in startscene

--- Assets/Scripts/GameStateDestroyer.cs
+++ Assets/Scripts/GameStateDestroyer.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManagerDestroyer : MonoBehaviour{
+public class GameStateDestroyer : MonoBehaviour {
+    
     // Start is called before the first frame update
     void Start()
     {
         Destroy(GameController.Instance);
+        Destroy(Player.Instance);
     }
 }
